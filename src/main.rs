0db47734a13c9db6ada0e5276fef011854fabfae
Sam Edelsten <samedelsten1@gmail.com>
2023-01-01 23:00:48 +0000

    fiziks

--- src/main.rs
+++ src/main.rs
@@ -1,128 +1,83 @@
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(bevy::diagnostic::LogDiagnosticsPlugin::default())
-        .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin::default())
-        .add_plugin(HelloPlugin)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .add_plugin(RapierDebugRenderPlugin::default())
+        .add_startup_system(setup_graphics)
+        .add_startup_system(setup_physics)
+        .add_startup_system(create_character_controller)
+        .add_system(print_ball_altitude)
+        .add_system(read_result_system)
+        .add_system(read_keyboard)
         .run();
 }
 
-#[derive(Component)]
-struct Velocity(Vec2);
-
-#[derive(Component)]
-struct Player;
-
-#[derive(Component)]
-struct Grounded(bool);
-
-#[derive(Component)]
-struct PhysObj;
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup_graphics(mut commands: Commands) {
+    // Add a camera so we can see the debug-render.
     commands.spawn(Camera2dBundle::default());
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("img/stikman.png"),
-            transform: Transform::from_xyz(100., 0., 0.),
+}
+
+fn setup_physics(mut commands: Commands) {
+    /* Create the ground. */
+    commands
+        .spawn(Collider::cuboid(500.0, 50.0))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)));
+
+    /* Create the bouncing ball. */
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::ball(50.0))
+        .insert(Restitution::coefficient(0.7))
+        .insert(TransformBundle::from(Transform::from_xyz(
+            100.0, 400.0, 0.0,
+        )));
+}
+
+fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
+    for transform in positions.iter() {
+        println!("Ball altitude: {}", transform.translation.y);
+    }
+}
+
+fn create_character_controller(mut commands: Commands) {
+    commands
+        .spawn(RigidBody::KinematicPositionBased)
+        .insert(Collider::ball(50.))
+        .insert(KinematicCharacterController {
+            apply_impulse_to_dynamic_bodies: true,
             ..default()
-        },
-        Velocity(Vec2 { x: (0.), y: (0.) }),
-        PhysObj,
-        Player,
-        Grounded(true),
-    ));
-
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("img/dvdlogo.png"),
-            transform: Transform::from_xyz(100., 0., 0.),
-            ..default()
-        },
-        PhysObj,
-        Velocity(Vec2 { x: (0.), y: (0.) }),
-    ));
+        })
+        .insert(TransformBundle::from(Transform::from_xyz(
+            -100.0, 10.0, 0.0,
+        )));
 }
 
-fn sprite_move(time: Res<Time>, mut sprite_position: Query<(&mut Velocity, &mut Transform)>) {
-    for (mut vel, mut transform) in &mut sprite_position {
-        transform.translation.x += vel.0.x * time.delta_seconds();
-        transform.translation.y += vel.0.y * time.delta_seconds();
-
-        // Bounds
-        if transform.translation.y > 200. {
-            // vel.0.y *= -1.;
-            transform.translation.y = 200.;
-        }
-
-        if transform.translation.y < -200. {
-            vel.0.y = 0.;
-            transform.translation.y = -200.;
-        }
-
-        if transform.translation.x > 200. {
-            // vel.0.x *= -1.;
-            transform.translation.x = 200.;
-        }
-
-        if transform.translation.x < -200. {
-            // vel.0.x *= -1.;
-            transform.translation.x = -200.;
-        }
+fn read_result_system(controllers: Query<(Entity, &KinematicCharacterControllerOutput)>) {
+    for (entity, output) in controllers.iter() {
+        println!(
+            "Entity {:?} moved by {:?} and touches the ground: {:?}",
+            entity, output.effective_translation, output.grounded
+        );
     }
 }
 
-fn gravity(mut phys_obj_vel: Query<&mut Velocity, With<PhysObj>>) {
-    for mut vel in &mut phys_obj_vel {
-        vel.0.y -= 18.2;
-    }
-}
-
-fn friction(mut phys_obj_vel: Query<&mut Velocity, With<PhysObj>>) {
-    for mut vel in &mut phys_obj_vel {
-        vel.0.x *= 0.9;
-    }
-}
-
-fn ground_set(mut grounds: Query<(&mut Grounded, &Transform)>) {
-    for (mut grounded, transform) in &mut grounds {
-        if transform.translation.y <= -200. {
-            grounded.0 = true;
-        }
-    }
-}
-
-fn keyboard_input(
-    kb_input: Res<Input<KeyCode>>,
-    mut vels: Query<(&mut Grounded, &mut Velocity, With<Player>)>,
+fn read_keyboard(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut players: Query<&mut KinematicCharacterController>,
+    output: Query<&KinematicCharacterControllerOutput>
 ) {
-    for (mut grounded, mut vel,_player) in &mut vels {
-        if kb_input.pressed(KeyCode::A) {
-            vel.0.x = -300.;
-        }
+    players.single_mut().translation = Some(Vec2::new(0., -0.9));
 
-        if kb_input.pressed(KeyCode::D) {
-            vel.0.x = 300.;
-        }
-
-        if kb_input.pressed(KeyCode::W) && grounded.0 {
-            vel.0.y = 666.;
-            grounded.0 = false;
-        }
+    if keyboard_input.pressed(KeyCode::A) {
+        players.single_mut().translation = Some(Vec2::new(-5., -0.9))
     }
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_startup_system(setup)
-            .add_system(keyboard_input)
-            .add_system(gravity)
-            .add_system(friction)
-            .add_system(ground_set)
-            .add_system(sprite_move);
+    if keyboard_input.pressed(KeyCode::D) {
+        players.single_mut().translation = Some(Vec2::new(5., -0.9))
+    }
+    if keyboard_input.pressed(KeyCode::Space) && output.single().grounded {
+        players.single_mut().translation = Some(Vec2::new(0., 150.))
     }
 }
