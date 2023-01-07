a26a57f68d8c5ded21ddde64ddd391ac22d8d185
Sam Edelsten <samedelsten1@gmail.com>
2023-01-07 22:38:27 +0000

    input control

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
+
+// TODO next: Higher jump for longer button press
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
-        .add_plugin(RapierDebugRenderPlugin::default())
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_startup_system(setup_graphics)
         .add_startup_system(setup_physics)
         .add_startup_system(create_character_controller)
-        .add_system(print_ball_altitude)
-        .add_system(read_result_system)
-        .add_system(read_keyboard)
+        // anything physicsy
+        .add_system_to_stage(CoreStage::PreUpdate, player_movement)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(1.0))
+        .add_plugin(RapierDebugRenderPlugin::default())
         .run();
 }
 
 fn setup_graphics(mut commands: Commands) {
+    let projection = OrthographicProjection {
+        scale: 1./6.,
+        ..default()
+    };
     // Add a camera so we can see the debug-render.
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle {
+        projection,
+        ..default()
+    });
 }
 
 fn setup_physics(mut commands: Commands) {
@@ -36,48 +47,97 @@ fn setup_physics(mut commands: Commands) {
         )));
 }
 
-fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
-    for transform in positions.iter() {
-        println!("Ball altitude: {}", transform.translation.y);
-    }
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct CCAcceleration(Vec2);
+
+#[derive(Component)]
+struct CCVelocity(Vec2);
+
+#[derive(Default)]
+struct DebugCount {
+    _n: usize,
 }
 
 fn create_character_controller(mut commands: Commands) {
     commands
         .spawn(RigidBody::KinematicPositionBased)
-        .insert(Collider::ball(50.))
+        .insert(Collider::ball(2.))
         .insert(KinematicCharacterController {
             apply_impulse_to_dynamic_bodies: true,
+            translation: Some(Vec2::ZERO),
             ..default()
         })
         .insert(TransformBundle::from(Transform::from_xyz(
             -100.0, 10.0, 0.0,
-        )));
+        )))
+        .insert(CCAcceleration(Vec2::new(0., 0.)))
+        .insert(CCVelocity(Vec2::new(0., 0.)))
+        .insert(Player);
 }
 
-fn read_result_system(controllers: Query<(Entity, &KinematicCharacterControllerOutput)>) {
-    for (entity, output) in controllers.iter() {
-        println!(
-            "Entity {:?} moved by {:?} and touches the ground: {:?}",
-            entity, output.effective_translation, output.grounded
-        );
-    }
-}
+const CC_JUMP_HEIGHT: f32 = 4.;
+const CC_GRAVITY: f32 = 0.2;
+const CC_WALKSPEED: f32 = 1.;
+const CC_WALKACCEL: f32 = 0.1;
+const CC_FRICTION_COEFFICIENT: f32 = 1.1;
 
-fn read_keyboard(
+fn player_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut players: Query<&mut KinematicCharacterController>,
-    output: Query<&KinematicCharacterControllerOutput>
+    mut player_info: Query<
+        (
+            &mut KinematicCharacterController,
+            &KinematicCharacterControllerOutput,
+            &mut CCAcceleration,
+            &mut CCVelocity,
+        ),
+        With<Player>,
+    >,
 ) {
-    players.single_mut().translation = Some(Vec2::new(0., -0.9));
+    for (mut controller, output, mut acc, mut vel) in &mut player_info {
+        // friction
+        if output.grounded {
+            vel.0.x /= CC_FRICTION_COEFFICIENT;
+        }
 
-    if keyboard_input.pressed(KeyCode::A) {
-        players.single_mut().translation = Some(Vec2::new(-5., -0.9))
-    }
-    if keyboard_input.pressed(KeyCode::D) {
-        players.single_mut().translation = Some(Vec2::new(5., -0.9))
-    }
-    if keyboard_input.pressed(KeyCode::Space) && output.single().grounded {
-        players.single_mut().translation = Some(Vec2::new(0., 150.))
+        let up = keyboard_input.any_just_pressed([KeyCode::W, KeyCode::Up]);
+        // let down = keyboard_input.any_pressed([KeyCode::S, KeyCode::Down]);
+        let left = keyboard_input.any_pressed([KeyCode::A, KeyCode::Left]);
+        let right = keyboard_input.any_pressed([KeyCode::D, KeyCode::Right]);
+
+        let x_axis = (-(left as i8) + right as i8) as f32 * CC_WALKACCEL;
+
+        let mut y_axis = if up && output.grounded {
+            CC_JUMP_HEIGHT
+        } else {
+            0.
+        };
+
+        if !output.grounded {
+            y_axis -= CC_GRAVITY;
+        }
+
+        acc.0 = Vec2::new(x_axis, y_axis);
+
+        if output.grounded && vel.0.y < 0. {
+            vel.0.y = 0.;
+            acc.0.y = 0.;
+        }
+
+        vel.0 += acc.0;
+
+        if vel.0.x > CC_WALKSPEED {
+            vel.0.x = CC_WALKSPEED;
+        }
+
+        if vel.0.x < -CC_WALKSPEED {
+            vel.0.x = -CC_WALKSPEED;
+        }
+
+        println!("V: {:?} | A: {:?}", vel.0, acc.0);
+
+        controller.translation = Some(vel.0);
     }
 }
